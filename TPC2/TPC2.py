0bcedcd3f8504e4874653ae7f065f5a426f082a2
Tomás Francisco A93193 <a93193@alunos.uminho.pt>
2023-02-28 11:33:06 +0000

    Update TPC2.py

--- TPC2/TPC2.py
+++ TPC2/TPC2.py
@@ -1,17 +1,33 @@
 def somador():
-    sum = 0
+    sum = i = 0
     switch = True
     received = input()
     input_string = list(received)
-    for i in range(len(input_string)):
+    limit = len(input_string)
+    while (i in range(0,limit)): # Melhorar isto para melhor desempenho.
         if (input_string[i] == 'o' or input_string[i] == 'O'):
-            if (input_string[i+1] == 'n' or input_string[i+1] == 'N') and (not switch):
+            if (input_string[i+1] == 'n' or input_string[i+1] == 'N'):
                 switch = True
-            elif (input_string[i+1] == 'f' or input_string[i+1] == 'F') and (input_string[i+2] == 'f' or input_string[i+2] == 'F') and (switch):
+                i += 2
+            elif (input_string[i+1] == 'f' or input_string[i+1] == 'F') and (input_string[i+2] == 'f' or input_string[i+2] == 'F'):
                 switch = False
+                i += 3
+            else:
+                i += 1
         elif (input_string[i].isnumeric() and switch):
-            sum = sum + int(input_string[i])
+            num = input_string[i]
+            n = i + 1
+            while (input_string[n].isnumeric()):
+                num += input_string[n]
+                n += 1
+                if (n >= limit):
+                    break
+            sum = sum + int(num)
+            i = n
         elif (input_string[i] == "="):
             print(sum)
+            i += 1
+        else:
+            i += 1
 
 somador()
